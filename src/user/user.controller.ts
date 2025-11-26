4a50665f912a614878000ddf0cb1a29d497864c7
Kader Kaya <kaader.kaayaa@gmail.com>
2025-11-26 21:50:44 +0300

    feat:added bcrypt and user endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
-  @Post()
-create(@Body() createUserDto: Prisma.UserCreateInput) {
-  return this.userService.create(createUserDto);
-}
-
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Post('sign-in')
+  async create(@Body() body: { email: string, password: string, name: string }) {
+    return this.userService.create(body.email, body.password, body.name);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Post('login')
+  async login(@Body() body: { email: string, password: string }) {
+    return this.userService.login(body.email, body.password);
+  }
+
+  @Get('getUserById')
+  async findOne(@Query('id') id: number) {
     return this.userService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: Prisma.UserUpdateInput) {
-    return this.userService.update(+id, updateUserDto);
+  @Post('update')
+  async update(@Body() body: { id: number, data: Prisma.UserUpdateInput }) {
+    return this.userService.update(body.id, body.data);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,30 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaClient, Prisma } from "@prisma/client";
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from "@prisma/client";
 import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcrypt';
+const saltOrRounds = 10;
 
 @Injectable()
 export class UserService {
-   constructor(private readonly databaseService: DatabaseService) { }
- async create(createUserDto: Prisma.UserCreateInput) {
-  return this.databaseService.user.create({
-    data:createUserDto
-  });
-}
-
-  findAll() {
-    return `This action returns all user`;
+  constructor(private readonly databaseService: DatabaseService) { }
+  async create(email: string, password: string, name: string) {
+    const hash = await bcrypt.hash(password, saltOrRounds);
+    console.log('hash', hash);
+    return this.databaseService.user.create({
+      data: {
+        email,
+        password: hash,
+        name
+      }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async login(email: string, password: string): Promise<any> {
+    const user = await this.getUser(email);
+    const hash = user!.password
+    const isMatch = await bcrypt.compare(password, hash);
+    if (!user) {
+      throw new Error('user not found')
+    }
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: number) {
+    return this.databaseService.user.findUnique({
+      where: { id }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getUser(email: string) {
+    return this.databaseService.user.findUnique({
+      where: { email }
+    });
   }
+
+  async update(id: number, data: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      where: { id },
+      data
+    })
+  }
+
 }
