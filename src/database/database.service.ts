4791a6feefc2e3904b44a12cde663120164d0ab5
Kader <kaderkaaya7@gmail.com>
2025-11-26 18:04:13 +0300

    fix:fixed internal error

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const adapter = new PrismaPg({connectionString});
+    super({ adapter });
+  }
+  
+
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
